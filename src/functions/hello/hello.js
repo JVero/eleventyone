066e8f3473b550a73b7710165ebf98a7ede044bc
joe vero <joev714@gmail.com>
2020-02-13 16:07:17 -0500

    added link to hello

--- src/functions/hello/hello.js
+++ src/functions/hello/hello.js
@@ -1,6 +1,7 @@
 exports.handler = async (event, context) => {
+  const link = event.queryStringParameters.link || "njace.us"
   try {
-    return { statusCode: 200, body: `Hello from a serverless function!` };
+    return { statusCode: 200, body: `this should go to ${link}!` };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
   }
